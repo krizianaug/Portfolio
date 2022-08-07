50595e5479bca989429a122ede551ed38b13565b
Krizian <krzaugusto@gmail.com>
2022-08-07 10:58:42 -0400

    modal and dark theme

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-    // emailjs
-    // .sendForm(
-    //     'service_twtahhs',
-    //     'template_v1ribgg',
-    //     event.target,
-    //     'QTktYluZ0k2105CYX'
-    // ).then(() => {
-    //     console.log('this worked!')
-    // })
+let isModalOpen = false;
+let contrastToggle = false;
+
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
+
+
+
+
+
 
 function contact(event){
     event.preventDefault();
@@ -14,10 +26,34 @@ function contact(event){
     const success = document.querySelector('.modal__overlay--success')
     loading.classList += " modal__overlay--visible";
 
-    setTimeout(() => {
+    
+    
+    emailjs
+    .sendForm(
+        'service_twtahhs',
+        'template_v1ribgg',
+        event.target,
+        'QTktYluZ0k2105CYX'
+    ).then(() => {
+        
         loading.classList.remove("modal__overlay--visible");
         success.classList += " modal__overlay--visible";
-        console.log('it worked 1')
-    }, 1000);
+        
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily available. Please contact me directly on kwb.augusto@gmail.com"
+        );
+    })
+
+}
+
+function toggleModal(){
+    document.body.classList += " modal--open"; 
+    isModalOpen = !isModalOpen;   
+    if (isModalOpen){
+        return document.body.classList.remove("modal--open")
+    } 
     
 }
+
